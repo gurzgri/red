gurzgri/red

commit 695eb71fdfecb3f6f76ed067d0e13464ea479978

FEAT: improvements to AS-COLUMN
- Is it my bug or does AS-COLUMN behave differently when compiled
  vs. interpreted? That needs to be examined.

environment/schemes/odbc.red
@@ -2455,11 +2455,15 @@ odbc: context [
 	as-column: function [column [string!]] [
 		upper: charset [#"A" - #"Z" #"À" - #"Ö" #"Ø" - #"Þ"]
 		lower: charset [#"a" - #"z" #"ß" - #"ö" #"ø" - #"ÿ"]
+		char:  union upper lower
+		digit: charset "1234567890"
 		parse/case column: system/words/copy column [
 			any [
 				change ["_" | "." | " "] "-"
-			|   here: upper upper lower (here: system/words/change/part here rejoin [here/1 here/2 "-" here/3] 3) :here
-			|   here: lower upper       (here: system/words/change/part here rejoin [here/1 "-" here/2] 2) :here
+			|   here: upper upper lower
+				(here: system/words/change/part here rejoin [here/1 here/2 "-" here/3] 3) :here
+			|   here: [lower upper | char digit | digit char]
+				(here: system/words/back system/words/change/part here rejoin [here/1 "-" here/2] 2) :here
 			|   skip
 			]
 		]

commit 957ca7bcbcee5150f89f51ffa515dc7657529022

FIX: fixes regression introduced with commit 695eb71

environment/schemes/odbc.red
@@ -2439,7 +2439,7 @@ odbc: context [
 
 		new-line/all collect [until [
 			system/words/change columns keep any [
-				attempt [to word! column: as-column second columns]
+				attempt [to word! as-column column: second columns]
 				column
 			]
 			new-line columns on